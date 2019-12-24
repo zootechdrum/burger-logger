1f00a538ecee6b222e8b5af6f7dc11b09d87883c
Cesar Gomez <zootechdrum@gmail.com>
2019-12-24 08:55:20 -0800

    added setup for index route in controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burger : data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,11 @@
 var orm = require("../config/orm");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  }
+}
+
+module.exports = burger;
