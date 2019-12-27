a7ab5e48f752b55cc889538666ff8d0402ddb54d
Cesar Gomez <zootechdrum@gmail.com>
2019-12-26 18:43:41 -0800

    add delete route in controller and delete function in orm

--- config/orm.js
+++ config/orm.js
@@ -2,12 +2,25 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-  all: function(tableInput, cb) {
+  all: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
+      cb(result);
+    });
+  },
+  delete: function (table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
       cb(result);
     });
   }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,4 +17,17 @@ router.get("/", function(req, res) {
   });
 });
 
+router.delete("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
 module.exports = router;
