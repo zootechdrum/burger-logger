743cf9df371de077ee5584b4661c0ef81005af0e
Cesar Gomez <zootechdrum@gmail.com>
2019-12-27 22:01:01 -0800

    add burger who have not been devoured

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder' , True);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double" , False);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator' , True);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Single" , False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Buttermilk Burger" , False);
