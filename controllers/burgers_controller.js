d176a3f319add131d2e7ab8d21525fe8ec7c5992
Cesar Gomez <zootechdrum@gmail.com>
2019-12-29 10:08:00 -0800

    added route handler to process updates to burgerstate

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -37,4 +37,25 @@ router.delete("/api/burger/:id", function(req, res) {
   });
 });
 
+router.put("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update(
+    {
+      burgerState: req.body.burgerState
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
 module.exports = router;
