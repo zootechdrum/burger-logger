4755035903b4e837fc3ba1d3a166faecb216157c
Cesar Gomez <zootechdrum@gmail.com>
2020-01-11 13:12:56 -0800

    added dotenv package globally

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-require("dotenv").config();
+require('dotenv').config()
 
 if (process.env.JAWSDB_URL){
     connection = 
     mysql.createConnection(process.env.JAWSDB_URL)
-}
+}else{
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -13,6 +13,7 @@ var connection = mysql.createConnection({
   password: "password",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
